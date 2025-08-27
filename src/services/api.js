2225a5e7ff653cf981b409eb0b71dd1725e9c504
Nayan <nayanmitra844@gmail.com>
2025-08-27 16:04:26 +0530

    adding netlify deploy file

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "b939e0ed429540d196d41c77cb76922e";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
